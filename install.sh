0123fe493808caf93494c4e1f5675b89a20d280f
Ali Hussain Abid <ali.hussain.abid.246@outlook.com>
2023-12-06 10:12:31 +0000

    Added install bash script

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Installing softwares that are needed for the setting up.
+sudo pacman -Syu && sudo pacman -Syyu && sudo pacman -S git ttf-firacode-nerd alacritty nemo rofi feh firefox
+
