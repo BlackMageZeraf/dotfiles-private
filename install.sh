44cee7cf914652493f070866cc2db05d0add88b6
Ali Hussain Abid <ali.hussain.abid.246@outlook.com>
2023-12-07 12:51:11 +0500

    added newer settings

--- install.sh
+++ install.sh
@@ -6,29 +6,34 @@ mkdir ~/Documents
 # Updating System to Latest
 sudo pacman -Syu && sudo pacman -Syu -y
 
-# Removing softwares that are preinstalled by arch linux that are not required in this Biased PC experience Setup.
-sudo pacman -R htop
-
 # Install softwares
-sudo pacman -S git ttf-firacode-nerd alacritty rofi feh firefox nvidia nvidia-utils nvidia-settings picom btop
+sudo pacman -S git ttf-firacode-nerd ttf-hack-nerd alacritty rofi feh firefox nvidia nvidia-utils nvidia-settings picom btop curl unzip man
+
+source ~/.bashrc
 
 # Installing Go Language
 # Go Version 1.21.5 Latest as per 7th December 2023
-wget https://go.dev/dl/go1.21.5.linux-amd64.tar.gz ~/Downloads
+wget https://go.dev/dl/go1.21.5.linux-amd64.tar.gz && mv go1.21.5.linux-amd64.tar.gz ~/Downloads
 sudo tar -C /usr/local -xzf ~/Downloads/go1.21.5.linux-amd64.tar.gz
 source ~/.bashrc
 go version
 
 # Installing Bun Better Javascript Runtime
-curl -fsSL https://bun.sh/install -y | bash
+curl -fsSL https://bun.sh/install | bash
 source ~/.bashrc
 bun -v
+
+# Installing Starship Prompt
+curl -fsSL https://starship.rs/install.sh | sh
 
 # Copying Settings files from repo to ~ directory
 cp .bashrc ~
 cp .alacritty.yml ~
 cp -r .config/ ~
 cp -r Pictures/ ~
+
+# Removing softwares that are preinstalled by arch linux that are not required in this Biased PC experience Setup.
+sudo pacman -R htop xterm 
 
 # Rebooting PC to fully Realize your settings.
 reboot
