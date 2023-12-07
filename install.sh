a0dc2633f5ff600b5e8f5375860094d281b3ea90
Ali Hussain Abid <ali.hussain.abid.246@outlook.com>
2023-12-07 10:34:19 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+mkdir ~/Downloads
+mkdir ~/Documents
+
 # Updating System to Latest
 sudo pacman -Syu && sudo pacman -Syu -y
 
-# Instal
+# Install softwares
 sudo pacman -S git ttf-firacode-nerd alacritty rofi feh firefox nvidia nvidia-utils nvidia-settings picom
+
+# Installing Go Language
+wget https://go.dev/dl/go1.21.5.linux-amd64.tar.gz ~/Downloads
+sudo tar -C /usr/local -xzf ~/Downloads/go1.21.5.linux-amd64.tar.gz
+source ~/.bashrc
+go version
+
+# Installing Bun Better Javascript Runtime
+curl -fsSL https://bun.sh/install -y | bash
+source ~/.bashrc
+bun -v
 
 # Copying Settings files from repo to ~ directory
 cp .bashrc ~
