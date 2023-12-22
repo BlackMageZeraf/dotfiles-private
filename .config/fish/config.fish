94fc31c2ee8089caaeafe0e4f1aba1116593bfb9
Ali Hussain Abid <ali.hussain.abid.246@outlook.com>
2023-12-22 23:21:13 +0500

    Restarting

--- .config/fish/config.fish
+++
@@ -1,5 +0,0 @@
-alias ll='ls -alFh --color=auto'
-alias ls='ls --color=auto'
-alias gitac='git add . && git commit -m'
-
-starship init fish | source
--- .config/i3/bar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Current Time and Date is: " $(date)
-while:
-do
-	sleep 1
-done
